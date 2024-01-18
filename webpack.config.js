467e993bfefd6a011c0d4fa76ad01e612eb0e622
Denis Beresnev <shanthi.bunch@gmail.com>
2024-01-19 02:42:05 +0300

    feat: Added base webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,72 +1,26 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const path = require('path');
 
 module.exports = (env, argv) => {
-  const isProduction = argv.mode === 'production';
-
-  const plugins = [
-    //   new CleanWebpackPlugin(),
-    new HtmlWebPackPlugin({
-      template: './src/index.html',
-      filename: './index.html',
-    }),
-    new MiniCssExtractPlugin({
-      filename: '[name].[hash].css',
-      chunkFilename: '[id].[hash].css',
-    }),
-  ];
-
-  if (isProduction) {
-    plugins.push(new CleanWebpackPlugin());
-  }
-
   return {
-    entry: './src/index.js',
+    entry: {
+      app: path.resolve(__dirname, 'src', 'index.js'),
+    },
     output: {
-      filename: 'bundle.[hash].js',
-      path: path.resolve(__dirname, 'dist'),
-      publicPath: '/dist/',
+      filename: '[name].[contenthash].js',
+      path: path.resolve(__dirname, 'build'),
+      clean: true,
     },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-          },
-        },
-        {
-          test: /\.html$/,
-          use: [
-            {
-              loader: 'html-loader',
-            },
-          ],
-        },
-        {
-          test: /\.css$/,
-          use: [
-            isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
-            'css-loader',
-          ],
-        },
-      ],
-    },
-    plugins,
+    plugins: [
+      new HtmlWebPackPlugin({
+        template: path.resolve(__dirname, 'public', 'index.html'),
+        filename: 'index.html',
+      }),
+    ],
     devServer: {
-      static: {
-        directory: path.join(__dirname, 'dist'),
-      },
-      compress: true,
-      port: 8080,
-      hot: true,
+      port: 3000,
       open: true,
-      historyApiFallback: true,
     },
-    // Это для улучшения карты исходников в разрабатываемых приложениях
-    devtool: isProduction ? 'source-map' : 'eval-source-map',
+    devtool: 'inline-source-map',
   };
 };
