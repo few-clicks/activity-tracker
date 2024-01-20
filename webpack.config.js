ed97fa8c10b4ae0a9505de566e5bb12269342344
Denis Beresnev <shanthi.bunch@gmail.com>
2024-01-20 15:00:07 +0300

    feat: Added sass-loader

--- webpack.config.js
+++ webpack.config.js
@@ -27,8 +27,8 @@ module.exports = (env, argv) => {
     module: {
       rules: [
         {
-          test: /\.css$/i,
-          use: ['style-loader', 'css-loader'],
+          test: /\.s[ac]ss$/i,
+          use: ['style-loader', 'css-loader', 'sass-loader'],
         },
       ],
     },
