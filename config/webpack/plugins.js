3c9055fb00af16ed748d8e5f369eb1e667122ee6
Denis Beresnev <shanthi.bunch@gmail.com>
2024-01-21 16:24:18 +0300

    feat: Added webpack bundle analyzer for prod

--- config/webpack/plugins.js
+++ config/webpack/plugins.js
@@ -1,9 +1,12 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const BundleAnalyzerPlugin =
+  require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const webpack = require('webpack');
 
 const getPlugins = ({ mode, paths }) => {
   const isDevelopment = mode === 'development';
+  const isProduction = mode === 'production';
 
   const plugins = [
     new HtmlWebPackPlugin({
@@ -17,6 +20,7 @@ const getPlugins = ({ mode, paths }) => {
   ];
 
   isDevelopment && plugins.push(new webpack.ProgressPlugin());
+  isProduction && plugins.push(new BundleAnalyzerPlugin());
 
   return plugins;
 };
