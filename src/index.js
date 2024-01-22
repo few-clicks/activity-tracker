22663fac3f6ce3baf613baf79b7c102b583f2452
Denis Beresnev <shanthi.bunch@gmail.com>
2024-01-22 21:05:47 +0300

    refactor: Removed unusual files

--- src/index.js
+++ src/index.js
@@ -1,9 +1,1 @@
-import './style.css';
-import styles from './style.module.scss';
-
-import '@/scripts/script';
-
-console.log('Hello, world', styles.test);
-
-const el = document.getElementById('test');
-el.classList.add('from_style');
+console.log('Hello, world');
