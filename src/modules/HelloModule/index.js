787013edbc4b0cabef9aefe4892d8aa05b0886f6
Denis Beresnev <shanthi.bunch@gmail.com>
2024-01-23 18:05:34 +0300

    feat: onClick in button, dispatch for inc and dec, subscribe click in hello module

--- src/modules/HelloModule/index.js
+++ src/modules/HelloModule/index.js
@@ -1,11 +1,23 @@
 import Header from '@/shared/Header';
 import styles from './style.module.css';
+import { store } from '../../app';
 
 export default () => {
   const element = document.createElement('div');
   element.classList.add(styles.hello);
 
   element.appendChild(Header('Hello, world!'));
+
+  const clicker = document.createElement('div');
+  clicker.style.fontSize = '26px';
+  clicker.style.marginLeft = '20px';
+  element.appendChild(clicker);
+
+  const updateValue = () => {
+    clicker.innerText = `Clicks: ${store.state.counter.value}`;
+  };
+  store.subscribe(updateValue);
+  updateValue();
 
   return element;
 };
