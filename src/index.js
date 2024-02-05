8446ce75352d2789ce9f77d852d477b0e5e46320
Denis Beresnev <shanthi.bunch@gmail.com>
2024-02-06 01:00:53 +0300

    feat: Added navigation elements and hash routes

--- src/index.js
+++ src/index.js
@@ -8,8 +8,42 @@ import {
   getCounterModule,
 } from './modules';
 
+const router = async () => {
+  const routes = {
+    '/': () => console.log('home'),
+    '/about': () => console.log('about'),
+    404: () => console.log('not found'),
+  };
+
+  if (location.hash === '') {
+    routes['/']();
+    return;
+  }
+
+  const route = location.hash.replace('#', '/');
+  if (routes[route]) {
+    routes[route]();
+  } else {
+    routes['404']();
+  }
+};
+
 window.addEventListener('load', () => {
   makeServiceWorkers();
+});
+
+//? for url routing
+// document.addEventListener('click', (event) => {
+//   const { target } = event;
+//   if (!target.matches('nav a')) {
+//     return;
+//   }
+//   // event.preventDefault();
+//   setTimeout(() => router(), 0);
+// });
+
+addEventListener('hashchange', (event) => {
+  router();
 });
 
 const rootElement = document.getElementById('root');
