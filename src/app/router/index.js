cedc97c5a09faac83c9769dde72e01ea8f8e39a5
Denis Beresnev <shanthi.bunch@gmail.com>
2024-02-06 23:57:28 +0300

    feat: Made routing through the class

---
+++ src/app/router/index.js
@@ -0,0 +1,43 @@
+class Router {
+  constructor(pageId = 'page', isHashRouter = true) {
+    !isHashRouter && console.log('url routing is not available now');
+    this.pageId = pageId;
+
+    window.addEventListener('hashchange', (event) => {
+      this._resolve();
+    });
+  }
+
+  initRoutes(routes) {
+    this._routes = routes;
+  }
+
+  route(route) {
+    if (this._routes[route]) {
+      return route;
+    }
+    console.error('This row is now exist:', route);
+    return '/';
+  }
+
+  navigate = (url) => {
+    history.pushState(null, null, url);
+  };
+
+  _resolve() {
+    if (location.hash === '') {
+      this._routes['/']();
+
+      return;
+    }
+
+    const route = location.hash.replace('#', '/');
+    if (this._routes[route]) {
+      this._routes[route]();
+    } else {
+      console.log('route not found');
+    }
+  }
+}
+
+export default new Router();
--- src/index.js
+++ src/index.js
@@ -7,43 +7,16 @@ import {
   getFormModule,
   getCounterModule,
 } from './modules';
-
-const router = async () => {
-  const routes = {
-    '/': () => console.log('home'),
-    '/about': () => console.log('about'),
-    404: () => console.log('not found'),
-  };
-
-  if (location.hash === '') {
-    routes['/']();
-    return;
-  }
-
-  const route = location.hash.replace('#', '/');
-  if (routes[route]) {
-    routes[route]();
-  } else {
-    routes['404']();
-  }
-};
+import router from './app/router';
 
 window.addEventListener('load', () => {
   makeServiceWorkers();
 });
 
-//? for url routing
-// document.addEventListener('click', (event) => {
-//   const { target } = event;
-//   if (!target.matches('nav a')) {
-//     return;
-//   }
-//   // event.preventDefault();
-//   setTimeout(() => router(), 0);
-// });
-
-addEventListener('hashchange', (event) => {
-  router();
+router.initRoutes({
+  '/': () => console.log('home'),
+  '/about': () => console.log('about'),
+  404: () => console.log('not found'),
 });
 
 const rootElement = document.getElementById('root');
