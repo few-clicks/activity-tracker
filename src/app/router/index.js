f2905449986f16bf7a0def1058d3dc4fa990cfc9
Denis Beresnev <shanthi.bunch@gmail.com>
2024-02-08 17:04:42 +0300

    refactor: Added _updatePage method to Router class

--- src/app/router/index.js
+++ src/app/router/index.js
@@ -21,18 +21,23 @@ class Router {
     this._pageContainer.id = 'page-container';
   }
 
+  _updatePage(element) {
+    this._pageContainer.innerHTML = '';
+    this._pageContainer.appendChild(element);
+  }
+
   _resolve() {
     if (location.hash === '') {
-      this._pageContainer.innerHTML = '';
-      this._pageContainer.appendChild(this._routes['/']);
-
+      const homePage = this._routes['/'];
+      if (homePage) {
+        this._updatePage(homePage);
+      }
       return;
     }
 
-    const route = location.hash.replace('#', '/');
-    if (this._routes[route]) {
-      this._pageContainer.innerHTML = '';
-      this._pageContainer.appendChild(this._routes[route]);
+    const newPage = this._routes[location.hash.replace('#', '/')];
+    if (newPage) {
+      this._updatePage(newPage);
     } else {
       this._pageContainer.innerHTML = '404 | Not Found';
     }
